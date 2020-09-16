9b5d4e5ddb0dd6a2b25298e46d03ef4323b700b2
mhull87 <melissahull87@gmail.com>
2020-09-16 14:52:12 -0500

    create portfolio

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const links = [{
+        label: 'Week 1',
+        url: 'week1/index.html'
+    },
+    {
+        label: 'Week 2',
+        url: 'week2/index.html'
+    }
+];
+
+
+links.forEach(element => {
+    var ol = document.querySelector('ol');
+    var li = document.createElement('li');
+    var a = document.createElement('a')
+    a.setAttribute('href', element.url);
+    ol.appendChild(li).appendChild(a).innerHTML = element.label;
+});
+
+
